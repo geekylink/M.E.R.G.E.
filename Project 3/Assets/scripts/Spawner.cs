c710132a1e35ed5bc5cb9099d486406013b1aaf5
kellydom <kellydom@umich.edu>
2014-10-26 15:02:27 -0400

    Enemy Spawner

---
+++ Project 3/Assets/scripts/Spawner.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+	public List<GameObject> enemiesToSpawn;
+
+	public float spawnTimer;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+
+	IEnumerator SpawnCoroutine(){
+		float timer = 0;
+		
+		while(timer < 1){
+			timer += Time.deltaTime * Time.timeScale / spawnTimer;
+			yield return 0;
+		}
+		
+		int randomEnemy = Random.Range (0, enemiesToSpawn.Count);
+		int side = Random.Range (0, 4);
+		
+		Vector2 posToSpawn = Vector2.zero;
+		if(side == 0){
+			posToSpawn.y = 1.1f; 
+			posToSpawn.x = Random.value;
+		}
+		else if(side == 1){
+			posToSpawn.y = -.1f;
+			posToSpawn.x = Random.value;
+		}
+		else if(side == 2){
+			posToSpawn.x = 1.1f;
+			posToSpawn.y = Random.value;
+		}
+		else{
+			posToSpawn.x = -.1f;
+			posToSpawn.y = Random.value;
+		}
+		posToSpawn = Camera.main.ViewportToWorldPoint(posToSpawn);
+		
+		GameObject enemyGO = Instantiate (enemiesToSpawn[randomEnemy]) as GameObject;
+		enemyGO.transform.position = posToSpawn;
+		enemyGO.transform.right = posToSpawn;
+		
+		StartCoroutine(SpawnCoroutine());
+	}
+
+	void Awake(){
+		StartCoroutine(SpawnCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
