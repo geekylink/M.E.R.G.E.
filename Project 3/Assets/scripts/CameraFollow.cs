97cc4eb0aac33eb3d7c7acc6911c89fae71a1b36
Ryan Wawrzaszek <ryanwawr@gmail.com>
2014-11-05 16:02:26 -0500

    Basic camera following and viewport management

---
+++ Project 3/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Slightly adapted from amaranth's code at
+//http://answers.unity3d.com/questions/29183/2d-camera-smooth-follow.html
+
+public class CameraFollow : MonoBehaviour {
+		
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public GameObject target;
+
+	void Awake(){
+		target = GameObject.FindGameObjectWithTag ("Target");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target.transform)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.transform.position);
+			Vector3 delta = target.transform.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+}
---
+++ Project 3/Assets/scripts/ViewportManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewportManager : MonoBehaviour {
+
+	public Camera P1Cam;
+	public Camera P2Cam;
+	public Camera P3Cam;
+	public Camera P4Cam;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake(){
+		GameObject[] cameras = GameObject.FindGameObjectsWithTag ("Camera");
+		foreach (GameObject cam in cameras) {
+
+			if(cam.camera.rect.x == 0 && cam.camera.rect.y == 0.5){
+				P1Cam = cam.camera;
+			} else if(cam.camera.rect.x == 0.5 && cam.camera.rect.y == 0.5){
+				P2Cam = cam.camera;
+			} else if(cam.camera.rect.x == 0 && cam.camera.rect.y == 0){
+				P3Cam = cam.camera;
+			} else if(cam.camera.rect.x == 0.5 && cam.camera.rect.y == 0){
+				P4Cam = cam.camera;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1")) {
+			Rect temp = new Rect (0,0, 0, 0);
+			P2Cam.rect = temp;
+			temp.width = 1;
+			temp.height = 0.5f;
+			P1Cam.rect = temp;
+		}
+		if (Input.GetKeyDown ("2")) {
+			Rect temp = new Rect (0,0, 0, 0);
+			P3Cam.rect = temp;
+			temp.width = 0.5f;
+			temp.height = 1;
+			P1Cam.rect = temp;
+		}
+	}
+}
