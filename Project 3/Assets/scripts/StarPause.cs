cbb7a0e7581a43f414098d9346b80d2e4f9ec78e
Brian Fulper <bfulper@umich.edu>
2014-11-08 22:21:24 -0500

    copy stars from my dev scene

---
+++ Project 3/Assets/scripts/StarPause.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarPause : MonoBehaviour {
+
+
+    float warmup = 5;
+	// Use this for initialization
+	void Start () {
+        //for(float s = 0; s < 5; s+=0.1f)
+        //    this.GetComponent<ParticleSystem>().Simulate(0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (warmup > 0)
+            warmup -= Time.deltaTime;
+        else
+        { 
+            this.GetComponent<ParticleSystem>().Pause();
+        }
+	}
+}
