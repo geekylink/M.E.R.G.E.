39caf0db3069e9ddab6ee79fc8ba0beebe52589d
kellydom <kellydom@umich.edu>
2014-11-10 13:39:47 -0500

    Added Splash Screen

---
+++ Project 3/Assets/scripts/SplashScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(InputManager.ActiveDevice.Action1){
+			Application.LoadLevel("dom-dev-temp");
+		}
+	}
+}
