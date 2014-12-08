fa8772004b694df18227123001ae80c4123749be
James Danielson <geekyguy2.0@gmail.com>
2014-12-07 23:18:58 -0500

    Controls Screen

---
+++ Project 3/Assets/scripts/ControlsScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class ControlsScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(InputManager.ActiveDevice.Action1){
+			Application.LoadLevel("PlayerSelectionScreen");
+		}
+	}
+}
