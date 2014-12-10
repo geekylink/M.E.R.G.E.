d6e593fb47dbcfe2f0576781c2217e53b3360927
James Danielson <geekyguy2.0@gmail.com>
2014-12-10 14:46:29 -0500

    Fixed some audiosource weirdness

--- Project 3/Assets/scripts/SFXManager.cs
+++ Project 3/Assets/scripts/SFXManager.cs
@@ -12,40 +12,37 @@ public class SFXManager : MonoBehaviour {
 	private AudioSource src;
 	private AudioSource[] sources;
 
+	private bool[] activeSrc;
 	private static int numChannels = 10;
 
+	// Use this for initialization
+	void Start () {
+		sources = new AudioSource[numChannels];
+		activeSrc = new bool[numChannels];
+		
+		for (int i = 0; i < numChannels; i++) {
+			sources[i] = this.gameObject.AddComponent ("AudioSource") as AudioSource;
+			sources[i].volume = 1;
+			activeSrc[i] = false;
+		}
+		
+		AddSound ("grenade", "grenade");
+		AddSound ("EndFX", "EndFX");
+	}
+
+	// Gets an instance of the sound manager
 	static public SFXManager getManager() {
 		GameObject obj = GameObject.Find ("SFXManager");
 		SFXManager manager = obj.GetComponent ("SFXManager") as SFXManager;
 		return manager;
 	}
 
+	// Plays sound "name"
 	public void playSound(string name) {
 		playList.Add (name);
 	}
 
-	// Use this for initialization
-	void Start () {
-		sources = new AudioSource[numChannels];
-
-		for (int i = 0; i < numChannels; i++) {
-			sources[i] = this.gameObject.AddComponent ("AudioSource") as AudioSource;
-			sources[i].volume = 1;
-		}
-
-		//AddSound ("explosion", "takeoff");
-		AddSound ("grenade", "grenade");
-		AddSound ("EndFX", "EndFX");
-		/*sources [0].clip = sounds [0].clip;
-		sources [1].clip = sounds [1].clip;
-		sources [0].Play ();
-		sources [1].Play ();*/
-		//playSound ("grenade");
-		//playSound ("EndFX");
-	}
-
-	//Sound sfx;
-
+	// Adds a sound that can then be played with playSound("name");
 	private void AddSound(string name, string resource) {
 		Sound sfx = new Sound ();
 		sfx.soundName = name;
@@ -55,32 +52,23 @@ public class SFXManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		while (playList.Count > 0) {
-			foreach (Sound snd in sounds) {
+		while (playList.Count > 0) { // Play all sounds in the playList
+			foreach (Sound snd in sounds) { // Finds the right AudioClip to load
 				if (snd.soundName == playList[0]) {
 
+					// Finds first available channel
 					for (int i = 0; i < numChannels; i++) {
 						if (!sources[i].isPlaying) {
 							sources[i].clip = snd.clip;
 							sources[i].Play();
+							break;
 						}
 					}
-					//src.clip = snd.clip;
-					//src.Play ();
+
 					playList.RemoveAt(0);
 					break;
 				}
 			}
-			//print (playList[0]);
-			//playList.RemoveAt(0);
-			/*foreach (Sound snd in sounds) {
-				if (snd.name == playList[0]) {
-					src.clip = snd.clip;
-					src.Play ();
-					playList.RemoveAt(0);
-					break;
-				}
-			}*/
 		}
 	}
 }
