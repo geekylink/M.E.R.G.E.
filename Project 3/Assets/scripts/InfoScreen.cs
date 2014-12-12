87632bb93c7ab600d644cf2468f59dfcfa42437f
Brian Fulper <bfulper@umich.edu>
2014-12-12 13:21:46 -0500

    new scenes

--- Project 3/Assets/scripts/InfoScreen.cs
+++ Project 3/Assets/scripts/InfoScreen.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 using InControl;
 
-public class SplashScreen : MonoBehaviour {
+public class InfoScreen : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
 	// Update is called once per frame
 	void Update () {
 		if(InputManager.ActiveDevice.Action1){
-			Application.LoadLevel("InfoScreen");
+			Application.LoadLevel("Controls");
 		}
 	}
 }
